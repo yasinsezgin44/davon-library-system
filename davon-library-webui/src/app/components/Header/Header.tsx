5670b9d1bc330229bd8b9e7cd6945fe84f799648
yasinsezgin44 <yasin.sezgin@davon.com.tr>
2025-04-30 11:53:56 +0300

    refactor: enhance Home component with new structure and placeholders
    - Updated the Home component in page.tsx to improve organization and clarity.
    - Added placeholder comments for future Hero and Features components to guide development.
    - Simplified the component structure for better maintainability.

---
+++ davon-library-webui/src/app/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import styles from "./Header.module.css";
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <header className="header-container">
+            <div className="logo-title-container">
+                <img src="images/logo.png" alt="Davon Library Logo" className="library-logo">
+                <h1>Davon Library</h1>
+            </div>
+
+            <input type="checkbox" id="menu-toggle" className="menu-toggle">
+            <label for="menu-toggle" className="menu-icon">☰</label>
+
+            <nav>
+                <ul className="nav-menu">
+                    <li className="nav-item"><a href="#home" className="nav-link">Home</a></li>
+                    <li className="nav-item"><a href="#about" className="nav-link">About</a></li>
+                    <li className="nav-item"><a href="#services" className="nav-link">Services</a></li>
+                    <li className="nav-item"><a href="#collection" className="nav-link">Collection</a></li>
+                    <li className="nav-item"><a href="#events" className="nav-link">Events</a></li>
+                    <li className="nav-item"><a href="#contact" className="nav-link">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
