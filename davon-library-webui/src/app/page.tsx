4cab27577da7033a18ddeca7690b6296c21f2df4
yasinsezgin44 <yasin.sezgin@davon.com.tr>
2025-04-30 15:23:06 +0300

    feat: add Footer component to Home page
    - Integrated the Footer component into the Home page to enhance the overall layout and user experience.
    - Updated the Home component structure to include the new Footer, maintaining a clean and organized design.
    - Ensured consistent use of relative paths for component imports.

--- davon-library-webui/src/app/page.tsx
+++ davon-library-webui/src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection/HeroSection"; // Use relative path
 import FeaturesSection from "./components/FeaturesSection/FeaturesSection"; // Use relative path
-
+import Footer from "./components/Footer/Footer"; // Use relative path
 export default function Home() {
   return (
     <>
       {" "}
       <HeroSection />
       <FeaturesSection />
+      <Footer />
     </>
   );
 }
