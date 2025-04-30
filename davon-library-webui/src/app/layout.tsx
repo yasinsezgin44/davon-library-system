66dd1427dd7b4eb10b9d4016dbca754a712b0110
yasinsezgin44 <yasin.sezgin@davon.com.tr>
2025-04-30 15:45:00 +0300

    feat: update layout with Header and Footer components
    - Integrated the Header and Footer components into the main layout for improved site structure and navigation.
    - Added a link to Font Awesome in the head for icon support.
    - Ensured consistent styling and organization within the layout component.

--- davon-library-webui/src/app/layout.tsx
+++ davon-library-webui/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "./components/Header/Header";
 import "./globals.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body className={inter.className}>
+        <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- davon-library-webui/src/app/page.tsx
+++ davon-library-webui/src/app/page.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection/HeroSection"; // Use relative path
 import FeaturesSection from "./components/FeaturesSection/FeaturesSection"; // Use relative path
-import Footer from "./components/Footer/Footer"; // Use relative path
 export default function Home() {
   return (
     <>
       {" "}
       <HeroSection />
       <FeaturesSection />
-      <Footer />
     </>
   );
 }
